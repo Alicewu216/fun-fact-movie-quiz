02379bf3d96f8e39aeeeaa788418c6bd2243dc32
Alice Wu <xiw451@ucsd.edu>
2020-10-22 01:13:08 -0700

    question options and answer variable added + Pseudo code in js

---
+++ script.js
@@ -0,0 +1,61 @@
+//variables
+
+//question object arrays with prompts, choices, and answer as object elements
+var questions = [
+  {
+    prompt: "1. What was the first movie in the Marvel Cinematic Universe?",
+    a: "Iron Man",
+    b: "Spider Man",
+    c: "Batman",
+    d: "The Avengers",
+    answer: "b",
+  },
+  {
+    prompt: "2. For which of these movies did Leonardo DiCaprio win an Oscar for Best Actor?",
+    a: "Blood Diamond",
+    b: "Titanic",
+    c: "The Revenant",
+    d: "The last King of Scotland",
+    answer: "c",
+  },
+  {
+    prompt: "3. In the 'Godfather' series, where was Vito Corleone born?",
+    a: "Corleone, Sicily",
+    b: "Athens",
+    c: "New York City",
+    d: "Rome",
+    answer: "a",
+  },
+  {
+    prompt: "4. Which one of these Academy Awards did Gone With the Wind not win?",
+    a: "Best actor",
+    b: "Best actress",
+    c: "Best picture",
+    d: "Best supporting actor",
+    answer: "a",
+  },
+  {
+    prompt: "5. In what yest is The Great Gastby set?",
+    a: "1925",
+    b: "1923",
+    c: "1924",
+    d: "1922",
+    answer: "d",
+  }
+];
+
+
+//call display function after clicking start
+
+//display question function
+//get current question (with maybe a for loop: terminate function at the end and allows restart)
+
+//display question
+//display options
+
+//check correctness of answer
+//compare user choice with answer
+//add score is anser match
+//call display function to display next question
+
+//timer function
